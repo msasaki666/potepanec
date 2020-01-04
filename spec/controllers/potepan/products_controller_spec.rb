01f47768dabcae2f1f0d5dbf527e07ddda6b8cdc
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-01-04 16:13:08 +0900

    task2 finished2

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Potepan::ProductsController, type: :controller do
   include ApplicationHelper
   describe "GET #show" do
     let(:product) { create(:base_product) }
+
     it "returns http success" do
       get :show, params: { id: product.id }
       expect(response).to have_http_status(:success)
     end
   end
-
 end
