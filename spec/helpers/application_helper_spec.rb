c7bf659bd50ab845cbc478ddb977eb2213935f82
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-01-13 22:51:22 +0900

    rubocopで修正

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "ApplicationHelper" do
   include ApplicationHelper
   describe "page title" do
     let(:product) { create(:custom_product) }
-    
+
     it "has correct title" do
       expect(full_title(product.name)).to eq "Custom Product | BIGBAG Store"
     end
