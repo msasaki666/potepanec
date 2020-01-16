0c700b88853f03d56d3932989bf718ebc3105c8f
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-01-16 15:54:59 +0900

    categories_controller.rbにてカテゴリー化された商品をインスタンス変数へ代入

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,4 +1,5 @@
 class Potepan::CategoriesController < ApplicationController
   def show
+    @categorized_products = Spree::Taxon.find(params[:id]).products || Spree::Taxon.all_products
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   namespace :potepan do
     get '/',                        to: 'sample#index'
     get 'index',                    to: 'sample#index'
-    get :product_grid_left_sidebar, to: 'sample#product_grid_left_sidebar'
     get :product_list_left_sidebar, to: 'sample#product_list_left_sidebar'
     get :single_product,            to: 'sample#single_product'
     get :cart_page,                 to: 'sample#cart_page'
