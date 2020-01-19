4ceabd804ece9acafdb316bc7004bbc97fbb3a60
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-01-19 16:14:16 +0900

    詳細ページのフィーチャースペック作成、リクエストスペック修正、n+1問題に対応

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,9 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxonomies = Spree::Taxonomy.all
     @taxon = Spree::Taxon.find(params[:id])
-    @products = @taxon.parent_id.nil? ? Spree::Product.all : Spree::Taxon.find(params[:id]).products
+    # rubocop:disable Style/MultilineTernaryOperator
+    @products = @taxon.parent_id.nil? ? Spree::Product.includes(master: [:images, :default_price])
+                                      : Spree::Taxon.find(params[:id]).products
+    # rubocop:enable Style/MultilineTernaryOperator
   end
 end
--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -3,19 +3,15 @@ require 'rails_helper'
 RSpec.describe "Potepan::Products", type: :request do
   include ApplicationHelper
   describe "product detail page" do
-    let(:product) { create(:custom_product) }
     let(:taxon) { create(:taxon) }
+    let!(:product) { create(:product, taxons: [taxon]) }
 
     before do
-      #get potepan_product_path(product.id)
+      get potepan_product_path(product.id)
     end
 
     it "returns http success" do
-    #  expect(response).to have_http_status(:success)
-    end
-
-    it "display product name" do
-    #  expect(response.body).to include "Custom Product"
+      expect(response).to have_http_status(:success)
     end
   end
 end
