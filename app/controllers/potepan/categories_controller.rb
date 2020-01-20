b01a8981e435ae24243fa7099243269584612451
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-01-20 18:54:23 +0900

    _category_side_bar.htmlでleavesメソッドを使用

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,9 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxonomies = Spree::Taxonomy.all
     @taxon = Spree::Taxon.find(params[:id])
-    # rubocop:disable Style/MultilineTernaryOperator
-    @products = @taxon.parent_id.nil? ? Spree::Product.includes(master: [:images, :default_price])
-                                      : Spree::Taxon.find(params[:id]).products
-    # rubocop:enable Style/MultilineTernaryOperator
+    @products = @taxon.parent_id.nil? ? Spree::Product.includes(master: [:images, :default_price]): @taxon.products
   end
 end
