a7afd535c453a7c7859603458588865d809a921a
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-01-20 19:16:59 +0900

    rubocop設定を変更、helpreメソッドの修正

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxonomies = Spree::Taxonomy.all
     @taxon = Spree::Taxon.find(params[:id])
-    @products = @taxon.parent_id.nil? ? Spree::Product.includes(master: [:images, :default_price]): @taxon.products
+    @products = @taxon.parent_id.nil? ? Spree::Product.includes(master: [:images, :default_price]) : @taxon.products
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def full_title(page_title = '') # rubocop:disable Style/OptArgParameters
+  def full_title(page_title = {})
     base_title = "BIGBAG Store"
     if page_title.blank?
       base_title
