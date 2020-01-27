dad3267dc403091173252f9b4a95059897519af3
Sasaki Motoaki <voyegesansretour1@gmail.com>
2020-01-28 00:31:49 +0900

    rubocopで修正

--- app/models/potepan/product_decorator.rb
+++ app/models/potepan/product_decorator.rb
@@ -1,7 +1,7 @@
 module Potepan::ProductDecorator
-    def related_products
-        Spree::Product.in_taxons(taxons).distinct.where.not(id: id)
-    end
-  
-    Spree::Product.prepend self
+  def related_products
+    Spree::Product.in_taxons(taxons).distinct.where.not(id: id)
   end
+
+  Spree::Product.prepend self
+end
